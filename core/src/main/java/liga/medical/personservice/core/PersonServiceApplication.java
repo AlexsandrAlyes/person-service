61f7fa243f3acd89213d06c611dd73745d9ce984
AlAles <alexs.al.ye.s@gmail.com>
2022-10-23 11:53:51 +0300

    change in pom

--- core/src/main/java/liga/medical/personservice/core/PersonServiceApplication.java
+++ core/src/main/java/liga/medical/personservice/core/PersonServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"liga.medical.personservice", "liga.medical.common.service"})
 public class PersonServiceApplication {
 
     public static void main(String[] args) {
